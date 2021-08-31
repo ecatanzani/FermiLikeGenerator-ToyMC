c1bd55ee66c1d2f125ca07528b71457423567325
Enrico Catanzani <18316072+ecatanzani@users.noreply.github.com>
2021-08-31 14:59:53 +0200

    Minor modifications

--- assets/condor_helper/helper.py
+++ assets/condor_helper/helper.py
@@ -5,22 +5,14 @@ def main(args=None):
     
     parser = ArgumentParser(description='HTCondor Job Status Controller - FermiLike ToyMC')
    
-    parser.add_argument("-o", "--output", type=str,
-                            dest='output', help='HTC output directory')
-    parser.add_argument("-e", "--evts", type=int, dest='evts',
-                            const=10000000, nargs='?', help='events to process in job')
-    parser.add_argument("-j", "--jobs", type=int, dest='jobs',
-                            const=1000, nargs='?', help='number of jobs')
-    parser.add_argument("-x", "--executable", type=str,
-                            dest='executable', help='ToyMC executable')     
-    parser.add_argument("-c", "--config", type=str,
-                            dest='config', help='Software Config Directory')                  
-    parser.add_argument("-v", "--verbose", dest='verbose', default=False,
-                        action='store_true', help='run in high verbosity mode')
-    parser.add_argument("-r", "--recreate", dest='recreate', default=False,
-                        action='store_true', help='recreate output dirs if present')
-    parser.add_argument("-s", "--status", dest='status', default=False,
-                        action='store_true', help='check output file status')
+    parser.add_argument("-o", "--output", type=str, dest='output', help='HTC output directory')
+    parser.add_argument("-e", "--evts", type=int, dest='evts', const=10000000, nargs='?', help='events to process in job')
+    parser.add_argument("-j", "--jobs", type=int, dest='jobs', const=1000, nargs='?', help='number of jobs')
+    parser.add_argument("-x", "--executable", type=str, dest='executable', help='ToyMC executable')     
+    parser.add_argument("-c", "--config", type=str, dest='config', help='Software Config Directory')                  
+    parser.add_argument("-v", "--verbose", dest='verbose', default=False, action='store_true', help='run in high verbosity mode')
+    parser.add_argument("-r", "--recreate", dest='recreate', default=False, action='store_true', help='recreate output dirs if present')
+    parser.add_argument("-s", "--status", dest='status', default=False, action='store_true', help='check output file status')
 
     opts = parser.parse_args(args)
 
